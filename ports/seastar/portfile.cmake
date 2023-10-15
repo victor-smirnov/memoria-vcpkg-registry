85fac3dfc1ddf306cd9ed65bb93f51151e46bbb0
Victor Smirnov <aist11@gmail.com>
2023-10-15 01:52:31 -0400

    Fixed Seastar building with new Boost

--- ports/seastar/portfile.cmake
+++ ports/seastar/portfile.cmake
@@ -4,17 +4,17 @@ vcpkg_from_github(
 	REF 4dc3871ed9f32816e7a03895e98c86a5502d980f
 	SHA512 22b762e6e5af38b48f7686f65ee62b11fb3d2a2ab8d5705d2437ad4ac177ff3fa4edfd4e6d249701b29f7b4af2d28dca0082a560248fc949f13ce60184ec1673
 	HEAD_REF master
+    PATCHES
+        fix-build.patch
 )
 
 vcpkg_configure_cmake(
 	SOURCE_PATH "${SOURCE_PATH}"
 	PREFER_NINJA
     OPTIONS
-        -DSeastar_DEMOS=OFF
         -DSeastar_TESTING=OFF
-        -DSeastar_APPS=OFF
-        -DSeastar_DOCS=OFF
         -DSeastar_SANITIZE=OFF
+        -DSeastar_CXX_FLAGS=-Wno-error
 )
 
 vcpkg_install_cmake()
